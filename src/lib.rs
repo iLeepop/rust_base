88df517d5833b1c3966bedfc3f30082a71996b4a
ileepop <zihao02325@gmail.com>
2025-05-13 14:09:58 +0800

    0513/thread init

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,6 @@
-mod base_flow_control;
-mod base_type;
-mod generic_type;
-mod traits;
-mod rule;
+mod base_flow_control;      // 控制流
+mod base_type;              // 基本类型
+mod generic_type;           // 泛型
+mod traits;                 // 特征
+mod rule;                   // 宏
+mod thread;                 // 线程
